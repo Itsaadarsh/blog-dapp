c8b9391f3a2373b186d8f428fd155e33674916e0
Itsaadarsh <heyitsaadarsh@gmail.com>
2022-04-27 12:04:03 +0530

    Adding test cases for Blog Contract

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,35 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Blog", function () {
+  it("Should create a post", async function () {
+    const Blog = await ethers.getContractFactory("Blog");
+    const blog = await Blog.deploy("Aadarsh Blog");
+    await blog.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    await blog.createPost("Learning Web3 Part 1", "This is my web3 blog!");
+    const posts = await blog.fetchPosts()
+    expect(posts[0].title).to.equal("Learning Web3 Part 1")
+  });
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  it("Should edit a post", async function () {
+    const Blog = await ethers.getContractFactory("Blog");
+    const blog = await Blog.deploy("Aadarsh Blog");
+    await blog.deployed();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    await blog.createPost("Learning Web3 prat 2", "This is my web3 blog prat 2!");
+    await blog.updatePost(1, "Learning Web3 Part 2", "This is my web3 blog part 2!", true);
+    const posts = await blog.fetchPosts()
+    expect(posts[0].title).to.equal("Learning Web3 Part 2")
+  });
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  it("Update blog name", async function () {
+    const Blog = await ethers.getContractFactory("Blog");
+    const blog = await Blog.deploy("Aadarsh Blg");
+    await blog.deployed();
+
+    expect(await blog.name()).to.equal("Aadarsh Blg")
+    await blog.updateName("Aadarsh Blog");
+    expect(await blog.name()).to.equal("Aadarsh Blog")
   });
 });
